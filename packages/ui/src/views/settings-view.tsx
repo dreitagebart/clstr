45d449546728b460ddad7f23a3d75f96d97cccd6
dreitagebart <stefan.buechold@gmail.com>
2024-02-22 00:48:52 +0100

    next

--- packages/ui/src/views/settings-view.tsx
+++ packages/ui/src/views/settings-view.tsx
@@ -9,32 +9,24 @@ import { FC } from 'react'
 interface Props {}
 
 export const SettingsView: FC<Props> = () => {
-  // const { data, error } = useSuspenseQuery(allUsersQuery, {
-  //   fetchPolicy: 'no-cache'
-  // })
-
-  // if (error) {
-  //   return (
-  //     <div>
-  //       <pre>{JSON.stringify(error, null, 2)}</pre>
-  //     </div>
-  //   )
-  // }
+  const { data, error } = useSuspenseQuery(allUsersQuery, {
+    fetchPolicy: 'no-cache'
+  })
 
   return (
-    <Auth>
+    <>
       <Header></Header>
       <Navbar>Einstellungen</Navbar>
       <>
-        {/* {data?.users.map((user) => {
+        {data?.users.map((user) => {
           return (
             <Group key={user.id}>
               <Text>{user.firstname}</Text>
               <Text>{user.lastname}</Text>
             </Group>
           )
-        })} */}
+        })}
       </>
-    </Auth>
+    </>
   )
 }
